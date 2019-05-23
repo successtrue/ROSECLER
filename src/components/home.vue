dabd9a4d25e3bd43a256acb42f7ff3f08e9f94bb
denghaopositive <49630593+denghaopositive@users.noreply.github.com>
2019-05-23 17:04:17 +0800

    Add files via upload

---
+++ src/components/home.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="main container-fluid">
+		<div class="row header">
+			<video width="320" height="240" controls autoplay>
+				<source src="../assets/home/header.mp4" type="video/mp4">
+			</video>
+			<div></div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Home'
+}
+</script>
+
+<style scoped>
+	*{ border: 1px solid red;}
+	.header{ 
+		height: 1080px;
+		background: url(../assets/home/header.png) no-repeat;
+		background-position: center;
+		}
+</style>
